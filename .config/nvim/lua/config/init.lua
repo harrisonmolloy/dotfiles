d84ce6add4a29d8676c5ed3aed3c3c93cb40d2f5
Harrison Knight Molloy <mail@harrisonmolloy.com>
2024-08-03 12:36:25 +1200

    feat: installs nvim-tree

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -28,6 +28,13 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- enable 24-bit colour
+vim.opt.termguicolors = true
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
