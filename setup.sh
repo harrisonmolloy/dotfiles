c7f476854b300a4d2211df6bb6e0c926ab3ee23b
Harrison Molloy <hello@harrisonmolloy.com>
2025-04-16 13:32:46 +1200

    feat: update setup scrip to support brew

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 !/usr/bin/env zsh
 
-## Fonts
+### install command line tools
+xcode-select --install
+
+### install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# install homebrew packages
+brew bundle install
+
+### Setup Fonts
 alias fontbook="open -b com.apple.FontBook"
 fontbook .config/fonts/SF-Mono-Nerd-Font-master/*.otf
 
-## Iterm 2
-
-# Specify the preferences directory
+### Setup Iterm 2
+## Specify the preferences directory
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.config/iterm2"
-# Tell iTerm2 to use the custom preferences in the directory
+## Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
